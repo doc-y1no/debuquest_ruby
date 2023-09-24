b7d52a50a031458132d19bd197a5714837b69d59
Apple <kisin1010@gmail.com>
2023-09-25 00:30:19 +0900

    勉強会にて回答までの考え方と一緒にご教示いただき完了

--- q4/calc.rb
+++ q4/calc.rb
@@ -1,7 +1,15 @@
 require "./base.rb"
 
 class Q4::Calc < Base
-  def self.twice(numbers)
-    numbers * 2
+  def self.twice(numbers)#どこを参照しているか、認識するようにする　。
+    # numbers * 2 #配列から要素を一つづつ取り出して、2倍にして、再度配列の値として追加する。
+    numbers2 = []
+    numbers.each do |number|
+      twice_number = number * 2 #ここの2倍の数字を11行目のnumberに入れていない 同じ名称は避ける
+      numbers2.push(twice_number)
+      #配列への代入の仕方
+
+    end#実際にできているか確認はループの外から確認
+    numbers2
   end
 end
